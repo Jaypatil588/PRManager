37932d4c6fc155425d7a011cbef9a2b551d0c1ce
Jay <jaypatil588@gmail.com>
2025-10-13 08:06:45 -0700

    check webhook data

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 from flask import Flask
-
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    print("Webhook received on app.py!")
+    return ('Webhook received on app.py!')
 
 if __name__ == '__main__':
     app.run(debug=True)
